ee33cff3026a57d6a54e891d586cbb9acf693b9c
Pengcheng Liu <pli49@cs18256kq.canterbury.ac.nz>
2018-10-16 16:21:54 +1300

    Led Matrix information

---
+++ gridmatrixinfor.c
@@ -0,0 +1,12 @@
+/** @file   gridmatrixinfor.c
+    @author Min Zhang & Bill Liu
+    @date   8 Oct 2018
+    @brief  matrix information.
+*/
+
+#include "gridmatrixinfor.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+
+
