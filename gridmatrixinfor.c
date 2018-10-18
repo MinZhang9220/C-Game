43c1489140ed43cc63faa9d53e353497043e5f85
Min Zhang <mzh73@cs18272kq.canterbury.ac.nz>
2018-10-19 12:26:43 +1300

    Led matrix information

--- gridmatrixinfor.c
+++ gridmatrixinfor.c
@@ -10,3 +10,23 @@
 
 
 
+/** Initialize matrix grid.
+@return matrix_grid*/
+Grid* initialize_matrix_grid(void)
+{
+    uint8_t row_n = 0;
+    uint8_t col_n = 0;
+    static Grid matrix_grid[35];
+    for (; row_n < 7;row_n++) {
+        for (; col_n < 5;col_n++) {
+            uint8_t index = row_n * 5 + col_n;
+            matrix_grid[index].row = row_n;
+            matrix_grid[index].column = col_n;
+            matrix_grid[index].received = 0;
+            matrix_grid[index].setted = 0;
+            matrix_grid[index].navswitched = 0;
+        }
+    }
+
+    return matrix_grid;
+}
